96b96c36946076bc39ca973f0ea96ccf0fc2e4e0
RomanWKus <RomanWKus@gmail.com>
2026-04-29 23:47:12 -0500

    Show selected item trash on desktop

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1108,8 +1108,11 @@ export default function Board() {
                 onClick={deleteSelectedDrawables}
               >
                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.3" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M18 6L6 18" />
-                  <path d="M6 6l12 12" />
+                  <polyline points="3 6 5 6 21 6" />
+                  <path d="M19 6l-1 14a2 2 0 01-2 2H8a2 2 0 01-2-2L5 6" />
+                  <path d="M10 11v6" />
+                  <path d="M14 11v6" />
+                  <path d="M9 6V4a1 1 0 011-1h4a1 1 0 011 1v2" />
                 </svg>
               </button>
               {canResizeSelection && (
